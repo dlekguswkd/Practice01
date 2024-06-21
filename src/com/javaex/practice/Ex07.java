97513910334a5234505a5bc1872d001c65ad901a
dlekguswkd <dlekguswkd@naver.com>
2024-06-21 14:36:03 +0900

    2024-06-21  추가

--- src/com/javaex/practice/Ex07.java
+++ src/com/javaex/practice/Ex07.java
@@ -1,7 +1,8 @@
 package com.javaex.practice;
 
 public class Ex07 {
-	/*다음 코드에서 변수로 사용할 수 없는 경우 이유를 작성하세요.
+	/*
+	 다음 코드에서 변수로 사용할 수 없는 경우 이유를 작성하세요.
 	(코드를 작성해서 오류가 없도록 변경해 보세요)
 	
 	public static void main(String[] args) {
@@ -16,11 +17,11 @@ public class Ex07 {
 	  
 	public static void main(String[] args) {
 		
-		int true;        //true는 예약어라서 식별자로 사용이 불가능
-		int #_of_workers;        //$와 _ 이외의 다른 특수문자는 사용 불가
+		int true;                     //true는 예약어라서 식별자로 사용이 불가능
+		int #_of_workers;             //$와 _ 이외의 다른 특수문자는 사용 불가
 		int countOfLetterslnString;
-		int 1stLevel1;           // 
-		int person#;          //$와 _ 이외의 다른 특수문자는 사용 불가
+		int 1stLevel1;                //글자는 int 사용불가 String 사용
+		int person#;                  //$와 _ 이외의 다른 특수문자는 사용 불가
 		int _person;
 		*/
 		
---
+++ src/com/javaex/practice/Hello.java
@@ -0,0 +1,19 @@
+package com.javaex.practice;
+
+     /*
+     아래 클래스 파일의 이름은 Hello.java 입니다.
+	 클래스의 코드가 아래와 같을 때 오류가 나는 부분을 찾고 이유를 설명해보세요
+public class hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");	
+
+	}
+
+}
+ 	답: 클래스의 이름이 서로 똑같아야함. 대문자,소문자도 똑같아야하기 때문에 오류가 생긴다
+ 	
+
+
+     */
